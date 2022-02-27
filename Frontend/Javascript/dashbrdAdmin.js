68520cf7229333f8f120f8a36c9db8cbb232c160
Sumit Kumar Shakya <sumitshakya20101999@gmail.com>
2022-02-27 14:33:33 +0530

    update

--- Frontend/Javascript/dashbrdAdmin.js
+++ Frontend/Javascript/dashbrdAdmin.js
@@ -135,74 +135,75 @@ const docList = [{
 
 function typedoc() {
     window.location.href = "./userServiceList.html";
-    let currdoc = 'docpurpose0'
+}
+let currdoc = 'docpurpose0'
 
-    window.onload = (event) => {
-        listofdoc()
-    }
+window.onload = (event) => {
+    listofdoc()
+}
 
-    function listofdoc() {
-        let innerHTM = ''
-        for (let i = 0; i < docList.length; i++) {
-            let temp = `<div id="docpurpose${i}" onclick="docClick(${i})" class="cardlist">
+function listofdoc() {
+    let innerHTM = ''
+    for (let i = 0; i < docList.length; i++) {
+        let temp = `<div id="docpurpose${i}" onclick="docClick(${i})" class="cardlist">
         ${docList[i].docPurpose}</div>`
-            innerHTM += temp
-        }
-        let temp1 = document.getElementById('docListId')
-        temp1.innerHTML = (innerHTM)
-        let curr = document.getElementById(currdoc)
-        console.log(curr)
-        curr.classList.add('currdoc')
-        docClick(0)
+        innerHTM += temp
     }
+    let temp1 = document.getElementById('docListId')
+    temp1.innerHTML = (innerHTM)
+    let curr = document.getElementById(currdoc)
+    console.log(curr)
+    curr.classList.add('currdoc')
+    docClick(0)
+}
 
-    function docClick(index) {
+function docClick(index) {
 
-        let curr = document.getElementById(currdoc)
-        console.log(curr)
-        curr.classList.remove('currdoc')
+    let curr = document.getElementById(currdoc)
+    console.log(curr)
+    curr.classList.remove('currdoc')
 
-        currdoc = 'docpurpose' + index
+    currdoc = 'docpurpose' + index
 
-        curr = document.getElementById(currdoc)
-        curr.classList.add('currdoc')
+    curr = document.getElementById(currdoc)
+    curr.classList.add('currdoc')
 
+    let innerHTM = '';
+    let len = docList[index].fieldsToVerify
+    for (let i = 0; i < len.length; i++) {
+        let temp = `<div class="cardlist">${len[i].fieldName}</div>`
+        innerHTM += temp
+    }
+    let temp1 = document.getElementById('docfieldId')
+    temp1.innerHTML = (innerHTM)
+}
+
+function addFieldClick(type) {
+    let t = document.getElementById(type)
+    if (t.innerText == 'Save Changes') {
+        t.innerHTML = 'Add New Field'
+    } else {
+        t.innerHTML = 'Save Changes'
+        console.log(t)
         let innerHTM = '';
-        let len = docList[index].fieldsToVerify
-        for (let i = 0; i < len.length; i++) {
-            let temp = `<div class="cardlist">${len[i].fieldName}</div>`
-            innerHTM += temp
-        }
-        let temp1 = document.getElementById('docfieldId')
-        temp1.innerHTML = (innerHTM)
     }
+}
 
-    function addFieldClick(type) {
-        let t = document.getElementById(type)
-        if (t.innerText == 'Save Changes') {
-            t.innerHTML = 'Add New Field'
-        } else {
-            t.innerHTML = 'Save Changes'
-            console.log(t)
-            let innerHTM = '';
-        }
-    }
+function adddocClick(type) {
+    let t = document.getElementById(type)
+    if (t.innerText == 'Save Changes') {
+        t.innerHTML = 'Add New Document'
 
-    function adddocClick(type) {
-        let t = document.getElementById(type)
-        if (t.innerText == 'Save Changes') {
-            t.innerHTML = 'Add New Document'
-
-            listofdoc()
-        } else {
-            t.innerHTML = 'Save Changes'
-            console.log(t)
-            let innerHTM = `<div class="cardlist inpt">
+        listofdoc()
+    } else {
+        t.innerHTML = 'Save Changes'
+        console.log(t)
+        let innerHTM = `<div class="cardlist inpt">
                             Type of Document :  <br>
                             <input type="text">
                         </div>`;
-            let temp1 = document.getElementById('docListId')
-            temp1.innerHTML = innerHTM
+        let temp1 = document.getElementById('docListId')
+        temp1.innerHTML = innerHTM
 
-        }
     }
+}
