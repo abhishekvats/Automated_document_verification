3e9149327b9fa1c67dea87c1a0779c70e28ba1ba
Bhanu Arora <mrbhanuarora@gmail.com>
2022-02-27 17:47:10 +0530

    image recog

--- Backend/imgRecognize.js
+++ Backend/imgRecognize.js
@@ -21,8 +21,8 @@
 const path = require("path");
 const aws = require("aws-sdk");
 aws.config.update({
-  secretAccessKey : "",
-  accessKeyId: "",
+  secretAccessKey : "fTOASGpC8ZswNFt6JCD4UJbBDEWz2w+TcXyvdqWU",
+  accessKeyId: "AKIAWZTTNVWRHABDP75A",
   region : "us-east-2"
 });
 const fs = require("fs");
@@ -30,7 +30,7 @@ const textRact = new aws.Textract({region : "us-east-2"});
 
 exports.imgRecog = (imageName) => {
   return new Promise((resolve,reject) => {
-    fs.readFile(path.join(__dirname , "public", imageName),{},(err,data) => {
+    fs.readFile(path.join(__dirname , "public", "ab.jpeg"),{},(err,data) => {
       if(err){
         console.log(err);
         return err;
@@ -47,12 +47,16 @@ exports.imgRecog = (imageName) => {
           return err;
         } 
         console.log(data1);
+        let data2 = data1.Blocks;
         let str = "";
-        for(let i = 0;i<data1.length ;i++){
-          if(data1[i].Text){
-            str += (data1[i].Text + " ") ;
+        for(let i = 0;i<data2.length ;i++){
+          if(data2[i].Text){
+            str += (data2[i].Text + " ") ;
+          }else{
+            console.log(data2[i].Geometry);
           }
         }
+        console.log(str);
         resolve(str);
       })
     } );
